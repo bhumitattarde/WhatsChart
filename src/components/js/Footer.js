787fc1ba58711b9b735f67305e768868f5d66390
Bhumit Attarde <bhumit.attarde01@gmail.com>
2021-05-16 20:01:08 +0530

    Added footer

--- src/components/js/Footer.js
+++ src/components/js/Footer.js
@@ -3,13 +3,18 @@ import React from "react";
 import "../css/Footer.css";
 
 const Footer = (props) => (
-  <div>
+  <div id="footer">
+    <hr />
     <ul>
-      <li>
+      <li id="donate">
         <a href="">Buy me a coffee!</a>
       </li>
       <li>
-        Made with &hearts; by <a href="">Bhumit Attarde</a>
+        Made with &hearts; by{" "}
+        <a href="https://www.bhumit.net">Bhumit Attarde</a>
+      </li>
+      <li>
+        &copy; 2021 <a href="https://www.bhumit.net">Bhumit Attarde</a>
       </li>
       <li>
         WhatsApp is a registered trademark of WhatsApp Inc. This site and
